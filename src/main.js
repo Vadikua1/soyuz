53d6d93a15ad932fa4bb8d908e0a29c156ee44e8
Vadikua1 <vadikuagitwork@ukr.net>
2026-03-28 20:07:09 +0200

    starting lab5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import posthog from 'posthog-js'
 
 createApp(App).mount('#app')
+
+posthog.init('phc_hfRRX7ZfDV4Tv8ln8GCAYE5ap3roF4UxraTBJLW2wJF', {
+    api_host: 'https://eu.posthog.com', //
+    person_profiles: 'always' //
+})
